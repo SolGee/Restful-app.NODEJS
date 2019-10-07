452a3a15fbd7bfcbd8ac88eaf9883c61f169bd17
SolGee <solsv00@hotmail.com>
2019-10-06 19:19:45 -0500

    Instalando d3-time desde npm

--- src/ejercicios/npm/index.js
+++ src/ejercicios/npm/index.js
@@ -1,10 +1,18 @@
+//Los meses empiezan a contarse desde 0. Enero es el mes 00.
+
 const start = new Date(2021, 00, 01);
 const end = new Date(2021, 00, 18);
 
-//Los meses empiezan a contarse desde 0. Enero es el mes 00.
+const newStart = new Date(2021, 02, 01);
+const newEnd = new Date(2021, 03, 01);
+
 
 const milSeconds = 24*60*60*1000;
 
-const resultados = (end-start)/milSeconds;
+const results = (end-start)/milSeconds;
 
-console.log(resultados);
+const newResults = (newEnd - newStart)/milSeconds;
+
+console.log(results);
+console.log(newResults);
+
